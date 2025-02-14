0092a92309613fddb724c2cd2b1b54505cb613a7
harshitha22112 <harshitaalakunta@gmail.com>
2025-02-14 12:55:51 +0530

    Add files via upload

---
+++ Movie Recommendation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+movies = pd.read_csv("Movie.csv")
+movies
+
+
+# In[ ]:
+
+
+
+
